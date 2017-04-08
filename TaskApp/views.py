3b45b1335470a85fae771196d047a1dc14dcb9fd
SATYENDRA-KUMAR-BANJARE <satyendrabanjare99@gmail.com>
2017-04-08 22:03:53 +0530

    Initial commit

---
+++ TaskApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .serializer import TaskSerializer
+from .models import Task
+
+class TaskViewSet(viewsets.ModelViewSet):
+
+    queryset = Task.objects.all().order_by('-date_created')
+    serializer_class = TaskSerializer
